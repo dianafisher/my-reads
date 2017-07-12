d06814e62a03a4018aa7592bfec63be80b4fce4b
Diana Fisher <diana.fisher@gmail.com>
2017-07-12 16:34:11 -0700

    Update title to MyReads.  Add components directory

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import * as BooksAPI from './utils/BooksAPI';
+import SearchBooks from './components/SearchBooks';
+import ListBooks from './components/ListBooks';
 
 class App extends Component {
+
+  state = {
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+    showSearchPage: false
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.showSearchPage ? (
+          <SearchBooks />
+        ) : (
+          <ListBooks />
+        )}
       </div>
     );
   }
---
+++ src/components/ListBooks.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ListBooks extends Component {
+  render() {
+    return (
+      <div className='list-books'>
+        <div className='list-books-title'>
+          <h1>MyReads</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ListBooks;
