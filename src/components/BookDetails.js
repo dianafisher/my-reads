61d4a647565b8cfbd6df27133fb2777360b236e2
Diana Fisher <diana.fisher@gmail.com>
2017-07-17 16:45:29 -0700

    Add styling to BookDetails

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -3,31 +3,49 @@ import * as BooksAPI from '../utils/BooksAPI';
 
 class BookDetails extends Component {
 
-  // constructor(props) {
-  //   super(props);
-  //
-  //   console.log(this.props);
-  //
-  // }
+  // initialize state
+  state = { details: {} };
 
   componentDidMount() {
     // the book id can be found in the params of the match object
     const bookId = this.props.match.params.id;
-    console.log('calling getBook for book id', bookId);
+
+    // get the book details
     this.getBook(bookId);
   }
 
   getBook = (bookId) => {
-    // call the BooksAPI to get the book by it's ID
+    // call the BooksAPI to get the book details by it's ID
     BooksAPI.get(bookId).then((book) => {
       console.log(book);
+      this.setState( {details: book} );
     });
   }
 
   render() {
+
+    const details = this.state.details;
+    console.log(details);
+    const thumbnail = details.imageLinks ? details.imageLinks.thumbnail : 'https://books.google.com/googlebooks/images/no_cover_thumb.gif';
+
     return (
       <div className='book-details'>
-        <div>Book Details...</div>
+        <div
+          className='book-details-cover'
+          style={{ width: 128, height: 193, backgroundImage: `url(${thumbnail})` }}
+        ></div>
+        <h3>Title</h3>
+        <div className='book-details-title'>{details.title}</div>
+        <h3>Description</h3>
+        <div className='book-details-description'>{details.description}</div>
+        <h3>Page Count</h3>
+        <div className='book-details-pages'>{details.pageCount} pages</div>
+        <h3>Author(s)</h3>
+        { details.authors && (
+          details.authors.map((author) => (
+            <div key={author}>{author}</div>
+          )))
+        }
       </div>
     )
   }
