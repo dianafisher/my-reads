4af21993b1dcd729154b27fd819227a5da983fbc
Diana Fisher <diana.fisher@gmail.com>
2017-07-22 20:29:11 -0700

    Make Bookshelf a stateless functional component

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,32 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types'
 import Book from './Book';
 
-class Bookshelf extends Component {
-
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    onUpdateBook: PropTypes.func.isRequired
-  }
-
-  render() {
-    return (
-      <div className='bookshelf'>
-        <h2 className='bookshelf-title'>
-          {this.props.title}
-        </h2>
-        <div className='bookshelf-books'>
-          <ol className='books-grid'>
-            {this.props.books.map((book) =>(
-              <li key={book.id} >
-                <Book data={book} onUpdateBook={this.props.onUpdateBook}></Book>
-              </li>
-            ))}
-          </ol>
-        </div>
+function Bookshelf (props) {
+  return (
+    <div className='bookshelf'>
+      <h2 className='bookshelf-title'>
+        {props.title}
+      </h2>
+      <div className='bookshelf-books'>
+        <ol className='books-grid'>
+          {props.books.map((book) =>(
+            <li key={book.id} >
+              <Book data={book} onUpdateBook={props.onUpdateBook}></Book>
+            </li>
+          ))}
+        </ol>
       </div>
-    )
-  }
+    </div>
+  )
+}
+
+Bookshelf.propTypes = {
+  title: PropTypes.string.isRequired,
+  onUpdateBook: PropTypes.func.isRequired
 }
 
 export default Bookshelf;
